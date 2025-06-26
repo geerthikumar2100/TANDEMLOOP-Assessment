5bdd84bea6a4e6e4c465f748df43b17ac316f294
Geerthana_J <geerthikumar2100@gmail.com>
2025-06-27 00:09:47 +0530

    Added - get N odds

--- Program-2.py
+++ Program-2.py
@@ -1,6 +1,5 @@
-def get_odd_series(a):
+def get_n_odds(n):
     result = []
-    for i in range(1, a + 1):
-        if i % 2 != 0:
-            result.append(i)
+    for i in range(n):
+        result.append(2 * i + 1)
     return result
